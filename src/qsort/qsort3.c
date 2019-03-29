65e7fa1fbc420318806be40e08c27d9555d8ba31
Freek Wiedijk <freek@cs.ru.nl>
2019-03-29 19:09:57 +0100

    some of Jean-Christophe's comments

--- src/qsort/qsort3.c
+++ src/qsort/qsort3.c
@@ -9,37 +9,44 @@ quicksort(double *base_ptr, int total_elems)
     return;
   lo = base_ptr;
   hi = lo + total_elems - 1;
-  mid = lo + ((hi - lo) >> 1);
-  if (*mid < *lo)
-    SWAP(mid, lo);
-  if (*hi < *mid) {
-    SWAP(mid, hi);
+  while (lo < hi) {
+    mid = lo + ((hi - lo) >> 1);
     if (*mid < *lo)
       SWAP(mid, lo);
-  }
-  left_ptr = lo + 1;
-  right_ptr = hi - 1;
-  do {
-    while (*left_ptr < *mid)
-      left_ptr++;
-    while (*mid < *right_ptr)
-      right_ptr--;
-    if (left_ptr < right_ptr) {
-      SWAP(left_ptr, right_ptr);
-      if (mid == left_ptr)
-        mid = right_ptr;
-      else if (mid == right_ptr)
-        mid = left_ptr;
-      left_ptr++;
-      right_ptr--;
-    } else if (left_ptr == right_ptr) {
-      left_ptr++;
-      right_ptr--;
-      break;
+    if (*hi < *mid) {
+      SWAP(mid, hi);
+      if (*mid < *lo)
+        SWAP(mid, lo);
     }
-  } while (left_ptr <= right_ptr);
-  quicksort(lo, right_ptr - lo + 1);
-  quicksort(left_ptr, hi - left_ptr + 1);
+    left_ptr = lo + 1;
+    right_ptr = hi - 1;
+    do {
+      while (*left_ptr < *mid)
+        left_ptr++;
+      while (*mid < *right_ptr)
+        right_ptr--;
+      if (left_ptr < right_ptr) {
+        SWAP(left_ptr, right_ptr);
+        if (mid == left_ptr)
+          mid = right_ptr;
+        else if (mid == right_ptr)
+          mid = left_ptr;
+        left_ptr++;
+        right_ptr--;
+      } else if (left_ptr == right_ptr) {
+        left_ptr++;
+        right_ptr--;
+        break;
+      }
+    } while (left_ptr <= right_ptr);
+    if (right_ptr - lo > hi - left_ptr) {
+      quicksort(left_ptr, hi - left_ptr + 1);
+      hi = right_ptr;
+    } else {
+      quicksort(lo, right_ptr - lo + 1);
+      lo = left_ptr;
+    }
+  }
 }
 
 int
